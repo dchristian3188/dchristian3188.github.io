9c3f14022c6535bf36c4ab890cc4d3ac3b05b462
dchristian3188 <dchristian3188@gmail.com>
2017-06-19 06:26:06 -0700

    playing with dynamic keyword

--- _drafts/Dyanmic Keywords.ps1
+++ _drafts/Dyanmic Keywords.ps1
@@ -1,15 +1,24 @@
+[System.Management.Automation.Language.DynamicKeyword]::Reset()
 $keyword = New-Object System.Management.Automation.Language.DynamicKeyword
 $keyword.Keyword ="MetaData"
 $keyword.BodyMode = [Management.Automation.Language.DynamicKeywordBodyMode]::HashTable
 $keyword.NameMode =  [Management.Automation.Language.DynamicKeywordNameMode]::NoName
-$keyword.DirectCall = $true
+#$keyword.DirectCall = $true
+#$keyword.ResourceName = 'IDK'
+#$keyword.ImplementingModule = 'Microsoft.PowerShell.Management'
+#$keyword.ImplementingModuleVersion  = '0.0.0.1'
+$PARAM = New-Object System.Management.Automation.Language.DynamicKeywordParameter
+$PARAM.Name="BoogersMcGee"
+$keyword.Parameters.Add($PARAM.Name,$PARAM)
 $prop = New-Object System.Management.Automation.Language.DynamicKeywordProperty
-$prop.Name="BoogersMcGee"
+$prop.Name="BoogersMcGeez"
 $prop.Mandatory = $true
 $keyword.Properties.Add($prop.Name,$prop)
 
 [System.Management.Automation.Language.DynamicKeyword]::AddKeyword($keyword)
+
 [System.Management.Automation.Language.DynamicKeyword]::GetKeyword()
+
 
 function MetaData
 {
@@ -17,15 +26,17 @@ function MetaData
         [Parameter(Mandatory)]
         $KeywordData,       # Not used in this function....
         [Parameter(Mandatory)]
-        [ScriptBlock]
+        [hashtable]
         $Value,             # The scriptblock that generates the configuration in each node.
         [Parameter(Mandatory)]
         $sourceMetadata     # Not used in this function
     )
-
+    
     $PSBoundParameters
+    
 }
 
- Metadata {
-     BoogersMcGee = "Monkey"
- }
+
+MetaData {
+    BoogersMcGeez = 'aadsfa'
+}
